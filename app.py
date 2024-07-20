41d6affee33cb65b3a7cf8bf09b8ec3854bb1613
unknown <=>
2024-07-20 23:25:41 +0530

    changed direct path

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def vector_embedding():
         st.session_state.embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
         
         # Corrected Path to PDF Directory
-        pdf_directory = ".\pdf_downloads"
+        pdf_directory = "./pdf_downloads"
         
         # Check if directory exists
         if not os.path.exists(pdf_directory):
